d40247051ef53ca85043a60b023ac23bb1444db8
Lucie Laporte <lucielefrapper@macbookpro.home>
2025-01-29 16:34:14 +0100

    changes on color'

--- rcf/tailwind.config.js
+++ rcf/tailwind.config.js
@@ -7,13 +7,39 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "rcf-blue": "#001575",
-        "rcf-orange": "#E07400",
+        "rcf-blue": {
+          50: "#f1f6ff",
+          100: "#e5edff",
+          200: "#cedeff",
+          300: "#a7c1ff",
+          400: "#7698ff",
+          500: "#3f64ff",
+          600: "#1838ff",
+          700: "#0729fa",
+          800: "#0522d2",
+          900: "#061dac",
+          950: "#001575",
+        },
+        "rcf-orange": {
+          50: "#fffbea",
+          100: "#fff3c5",
+          200: "#ffe687",
+          300: "#ffd348",
+          400: "#ffbe1e",
+          500: "#fc9c04",
+          600: "#e07400",
+          700: "#b94f04",
+          800: "#963d0a",
+          900: "#7b320c",
+          950: "#471801",
+        },
         "rcf-grey": "#E7ECEF",
         "rcf-black": "#0A0903",
       },
       fontFamily: {
-        text: "Caveat",
+        sans: ["Tahoma", "Arial", "sans-serif"], // Remplace la police sans-serif
+        cursive: ["Brush Script MT"],
+        mono: ["Courier New", "monospace"],
       },
     },
   },
